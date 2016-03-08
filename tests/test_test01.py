3cc80240b3f58a76600c8ba3d50a869ce2466fed
Mehdi Hatef <mehdi.207@gmail.com>
2016-03-08 10:42:15 +0100

    add hello world functionality

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -11,12 +11,17 @@ Tests for `test01` module.
 import unittest
 
 import test01
-
+import cli
 
 class TestTest01(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.hello_message = "hello, Mehdi!"
+        #pass
+
+    def test_prints_hello_mehdi(self):
+        output = cli.hello()
+        assert(output == self.hello_message)
 
     def test_something(self):
         assert(test01.hello_world())
